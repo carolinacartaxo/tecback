58709c71e770aafec284faa7f31f7092888d81d1
carolcartaxo <carolinamcartaxo@gmail.com>
2025-03-18 20:44:43 -0300

    Aula - 18/03

--- src/main/java/br/uniesp/si/techback/controller/FilmeController.java
+++ src/main/java/br/uniesp/si/techback/controller/FilmeController.java
@@ -3,7 +3,9 @@ package br.uniesp.si.techback.controller;
 
 import br.uniesp.si.techback.model.Filme;
 import br.uniesp.si.techback.repository.FilmeRepository;
+import br.uniesp.si.techback.service.FilmeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,16 +14,28 @@ import java.util.List;
 @RequestMapping("/filmes")
 public class FilmeController {
 
-    @Autowired //Iremos remover essa anotação na próxma aula
-    private FilmeRepository filmeRepository;
+    @Autowired
+    private FilmeService service;
+
 
     @GetMapping
     public List<Filme> listar(){
-        return filmeRepository.findAll();
+        return service.listar();
     }
 
     @PostMapping
-    public Filme inserir(@RequestBody Filme filme){
-        return filmeRepository.save(filme);
+    public Filme incluir(@RequestBody Filme filme){
+        return service.cadastrar(filme);
+    }
+
+    @PutMapping
+    public Filme alterar(@RequestBody Filme filme){
+        return service.alterar(filme);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable Integer id){
+        service.excluir(id);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/br/uniesp/si/techback/repository/FilmeRepository.java
+++ src/main/java/br/uniesp/si/techback/repository/FilmeRepository.java
@@ -2,6 +2,9 @@ package br.uniesp.si.techback.repository;
 
 import br.uniesp.si.techback.model.Filme;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface FilmeRepository extends JpaRepository<Filme, Integer> {
+@Repository
+public interface FilmeRepository
+                    extends JpaRepository<Filme, Integer> {
 }
